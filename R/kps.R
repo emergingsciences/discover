9c75e43d890651e7482dc6b57961408185159c0d
lawof1 <lawof1@emergingsciences.org.local>
2016-08-22 13:21:51 -0500

    Kundalini Profile Survey analysis examples
    Introduced parallel analysis and numerous (modular) code examples.
    Corresponds to information found in the Kundalini Profile Survey
    Analysis Guide.

--- R/kps.R
+++ R/kps.R
@@ -31,48 +31,6 @@ kps.get.questiontext <- function(x = kps.loaddatafile()) {
 
 
 
-# polyfa()
-# 
-# Outputs the following, in order:
-# 1. Descriptive statistics
-# 2. Eigenvalues
-# 3. Scree plot and parallel analysis (plot)
-# 4. Factor loadings
-# 5. Phi matrix
-# 6. Factor plot (plot)
-#
-# Credit: https://blogs.baylor.edu/grantmorgan/2013/03/08/click-here-for-exploratory-factor-analysis-code/
-#
-kps.polyfa <- function(x, nfactors = NULL) {
-  
-  # Descriptive stats to ensure quality
-  
-  x.num <- as.data.frame(lapply(x, as.numeric))
-  print(describe(x.num)) # Output descriptive statistics
-  
-  # See eigenvalues
-  
-  x.temp <- fa.poly(x = x.num, fm = "pa")
-  print(x.temp$fa$values)  # Output eigenvalues
-  
-  # Polychoric correlations viz to determine # of factors
-  
-  x.poly <- polychoric(x.num)
-  x.ev <- eigen(x.poly$rho)
-  plotnScree(nScree(x.ev$values), main = "Scree Plot & Parallel Analysis")
-  
-  # Extract factors based on scree
-  if(is.null(nfactors)) {
-    warning("nfactors not set. Will not generate loadings table, phi matrix, or factor plot.")
-  } else {
-    x.out <- fa.poly(x = x.num, fm = "pa", nfactors = nfactors, rotate = "promax", residual = TRUE)
-    print(x.out$fa$loadings)  # The factor loadings table, or the "strength" of a variable for a factor
-    print(x.out$fa$Phi)  # The phi matrix represents correlations between factors
-    factor.plot(x.out$fa)    
-  }
-}
-
-
 # Create sample plot for sex of participants
 kps.sample.histplot <- function() {
   kps.data <- kps.loaddatafile()
